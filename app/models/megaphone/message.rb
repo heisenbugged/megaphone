044c3846b65a21b8538c50303cf6a02137fc5a8b
James Strong <laspluviosillas@gmail.com>
2014-08-19 08:40:33 -0500

    Add delegate and scope.

--- app/models/megaphone/message.rb
+++ app/models/megaphone/message.rb
@@ -6,8 +6,11 @@ class Megaphone::Message < ActiveRecord::Base
   belongs_to :category
   belongs_to :notifiable, polymorphic: true
 
+  delegate :name, to: :category, prefix: true
+
   # == Scopes ==
   default_scope includes(:category)
   scope :recent, -> { where("megaphone_messages.created_at > ?", 1.month.ago) }
   scope :unread, -> { where(read: false) }
+  scope :read, -> { where(read: true) }
 end
